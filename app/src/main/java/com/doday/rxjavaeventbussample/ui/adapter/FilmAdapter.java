b749a50a2c7c4ed03e30a246bed692103bb4feeb
doday <sessi.amedie@gmail.com>
2016-12-30 17:27:37 +0100

    replace listview by recyclerview

---
+++ app/src/main/java/com/doday/rxjavaeventbussample/ui/adapter/FilmAdapter.java
@@ -0,0 +1,91 @@
+package com.doday.rxjavaeventbussample.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.doday.rxjavaeventbussample.R;
+import com.doday.rxjavaeventbussample.model.Film;
+import com.doday.rxjavaeventbussample.util.DateUtils;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by sessi on 19/10/16.
+ */
+
+public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.ViewHolder> {
+
+
+    private ArrayList<Film> films =  new ArrayList<>();
+    private static final DateUtils dateUtils = DateUtils.getInstance();
+    private View.OnClickListener mOnClickListener;
+
+    public FilmAdapter(Context context, ArrayList<Film> films) {
+        this(context);
+        this.films = films;
+    }
+
+    public FilmAdapter(Context context) {
+    }
+
+
+    public void setFilms(ArrayList<Film> films) {
+        this.films = films;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_film, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(films.get(position).title);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return films.size();
+    }
+
+    public void addFilm(Film film) {
+        films.add(film);
+    }
+
+    public void clearAdapter() {
+        films.clear();
+        notifyDataSetChanged();
+    }
+
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.mOnClickListener = onClickListener;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.list_film_title);
+
+            if(mOnClickListener != null) {
+                itemView.setOnClickListener(mOnClickListener);
+            }
+        }
+    }
+}
--- app/src/main/java/com/doday/rxjavaeventbussample/ui/fragment/FilmListFragment.java
+++ app/src/main/java/com/doday/rxjavaeventbussample/ui/fragment/FilmListFragment.java
@@ -1,12 +1,13 @@
 package com.doday.rxjavaeventbussample.ui.fragment;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.doday.rxjavaeventbussample.EventBus;
 import com.doday.rxjavaeventbussample.MyApplication;
@@ -15,8 +16,9 @@ import com.doday.rxjavaeventbussample.event.ListFilmEvent;
 import com.doday.rxjavaeventbussample.manager.FilmManager;
 import com.doday.rxjavaeventbussample.model.Film;
 import com.doday.rxjavaeventbussample.ui.activity.DetailFilmActivity;
-import com.doday.rxjavaeventbussample.ui.adapter.FilmBaseAdapter;
+import com.doday.rxjavaeventbussample.ui.adapter.FilmAdapter;
 import com.doday.rxjavaeventbussample.util.DateUtils;
+import com.doday.rxjavaeventbussample.viewmodel.RecyclerItemClickListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -31,27 +33,60 @@ import rx.android.schedulers.AndroidSchedulers;
  * Created by sessi on 20/10/16.
  */
 
-public class FilmListFragment extends ListFragment {
+public class FilmListFragment extends Fragment {
 
     private final DateUtils dateUtils = DateUtils.getInstance();
     private ArrayList<Film> films = new ArrayList<>();
-    private FilmBaseAdapter filmBaseAdapter;
+    private FilmAdapter mFilmAdapter;
     private Observable<ListFilmEvent> filmsObservable;
     private Subscription subscription;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_films, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.list_film);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mFilmAdapter = new FilmAdapter(getActivity());
+        mRecyclerView.setAdapter(mFilmAdapter);
+
+        initClickListener(mRecyclerView);
+
         return view;
+
+    }
+
+
+    private void initClickListener(final RecyclerView recyclerView) {
+        RecyclerItemClickListener.OnItemClickListener l = (new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //int position = recyclerView.getChildLayoutPosition(view);
+                DetailFilmActivity.startDetailFilm(getActivity(), films.get(position));
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+
+        });
+
+        RecyclerItemClickListener onRecyclerMutualFriendClickListener = new RecyclerItemClickListener(getActivity()
+                , recyclerView
+                , l);
+        recyclerView.addOnItemTouchListener(onRecyclerMutualFriendClickListener);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-
-        filmBaseAdapter = new FilmBaseAdapter(getActivity());
-        setListAdapter(filmBaseAdapter);
         filmsObservable = EventBus.sAppBus.observeEvents(ListFilmEvent.class);
 
     }
@@ -61,20 +96,20 @@ public class FilmListFragment extends ListFragment {
     @Override
     public void onStart() {
         super.onStart();
-        requestFilms(filmBaseAdapter);
+        requestFilms(mFilmAdapter);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        filmBaseAdapter.clearAdapter();
+        mFilmAdapter.clearAdapter();
         filmsObservable.unsubscribeOn(AndroidSchedulers.mainThread());
         this.subscription.unsubscribe();
         //TODO annuler la requête
 
     }
 
-    private void requestFilms(final FilmBaseAdapter filmBaseAdapter) {
+    private void requestFilms(final FilmAdapter filmAdapter) {
 
         try {
             FilmManager.getInstance(MyApplication.getAppContext()).getFilmsWS();
@@ -96,8 +131,8 @@ public class FilmListFragment extends ListFragment {
                             public void onNext(ListFilmEvent films) {
                                 Log.d(getClass().getSimpleName(), "onNext film récupéré via rxjava : " + films);
                                 FilmListFragment.this.films = films;
-                                filmBaseAdapter.setFilms(films);
-                                filmBaseAdapter.notifyDataSetChanged();
+                                filmAdapter.setFilms(films);
+                                filmAdapter.notifyDataSetChanged();
                             }
                         });
 
@@ -108,12 +143,6 @@ public class FilmListFragment extends ListFragment {
 
     }
 
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        DetailFilmActivity.startDetailFilm(getActivity(), films.get(position));
-    }
 
 
 }
